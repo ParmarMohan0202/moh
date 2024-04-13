0b983d19f68026c906b63c681ff6d6415c37fb62
ParmarMohan0202 <166895464+ParmarMohan0202@users.noreply.github.com>
2024-04-13 11:38:50 +0530

    Add files via upload

---
+++ cie1.py
@@ -0,0 +1,32 @@
+print("enter your marks obtained in 5 subject:")
+markone=int(input())
+marktwo=int(input())
+markthree=int(input())
+markfour=int(input())
+markfive=int(input())
+tot=markone+marktwo+markthree+markfour+markfive
+avg=tot/5
+if avg>=90 and avg<=100:
+    print("your grade is A1")
+elif avg>=81 and avg<91:
+    print("your grade is A2")
+elif avg>=71 and avg<81:
+    print("your grade is B1")
+elif avg>=61 and avg<71:
+    print("your grade is B2")
+elif avg>=51 and avg<61:
+    print("your grade is C1")
+elif avg>=41 and avg<51:
+     print("your grade is C2")
+elif avg>=31 and avg<41:
+     print("your grade is D")
+elif avg>=21 and avg<31:
+     print("your grade is E1")
+elif avg>=0 and avg<21:
+     print("your grade is E2")
+ else 
+     print('invaild input')
+    
+    
+    
+    
